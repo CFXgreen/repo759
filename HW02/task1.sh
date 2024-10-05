9521b2f28cd0d051fbea50d6e7e1abb7e05ea3e5
CFXgreen <129789314+CFXgreen@users.noreply.github.com>
2024-10-05 00:02:55 -0500

    Add files via upload

--- HW02/task1.sh
+++ HW02/task1.sh
@@ -1,2 +1,19 @@
-#!/bin/bash
-echo "Hello, World!"
+#!/usr/bin/bash
+#SBATCH -p instruction
+#SBATCH --job-name=task1
+#SBATCH -o task1-%j.out -e task1-%j.err
+#SBATCH --time=0-00:04:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=10
+#SBATCH --cpus-per-task=1
+cd $SLURM_SUBMIT_DIR
+# load the gcc for compiling C++ programs
+module load gcc/13.2.0
+# load the nvcc for compiling cuda programs
+module load nvidia/cuda
+# clone (replace the github link to yours)
+git clone https://github.com/CFXgreen/repo759.git
+cd repo759/HW02
+g++ scan.cpp task1.cpp -Wall -O3 -std=c++17 -o task1
+
+./task1 2048
